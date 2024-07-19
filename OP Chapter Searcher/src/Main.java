dc9b1652f9aaf3b5aba59ba696dbf6f73b1fcf48
NithishRajMR <nithishrajmr@gmail.com>
2024-07-19 14:01:42 +0530

    added the basic saga class and its declaration.

--- OP Chapter Searcher/src/Main.java
+++ OP Chapter Searcher/src/Main.java
@@ -1,52 +1,34 @@
 import java.util.Scanner;
 
 
-class DataOfChapter{
-    int chapter;
+class Saga{
+    int startingchapter;
+    int endingchapter;
+    String saganame;
 
-    DataOfChapter(int chapter){
-        this.chapter = chapter;
-    }
-    void checkChapterSaga() {
-        if (chapter >= 1 && chapter <= 61) {
-            System.out.println(chapter + " is East Blue saga");
-        } else if (chapter >= 61 && chapter <= 135) {
-            System.out.println(chapter + " is Arabasta saga");
-        } else if (chapter >= 135 && chapter <= 206) {
-            System.out.println(chapter + " is Sky Island saga");
-        } else if (chapter >= 206 && chapter <= 325) {
-            System.out.println(chapter + " is Water 7 saga");
-        } else if (chapter >= 325 && chapter <= 384) {
-            System.out.println(chapter + " is Thriller Bark saga");
-        } else if (chapter >= 384 && chapter <= 492) {
-            System.out.println(chapter + " is Summit War saga");
-        } else if (chapter >= 492 && chapter <= 542) {
-            System.out.println(chapter + " is Fishman Island saga");
-        }else if (chapter>=542 && chapter <= 746) {
-            System.out.println(chapter + " is Dressrosa saga");
-        }else if (chapter>=746 && chapter <= 889) {
-            System.out.println(chapter + " is Whole cake Island saga");
-        }else if (chapter>=889 && chapter <= 1030) {
-            System.out.println(chapter + " is Wona Country saga");
-        }else if (chapter>=1030 && chapter <= 1086) {
-            System.out.println(chapter + " is Final saga");
-        }else if (chapter >  1086) {
-            System.out.println("TO BE CONTINUED");
-        }
-        else{
-            System.out.println("ERROR:INVALID INPUT");
-        }
+    Saga(int StartingChapter, int EndingChapter, String SagaName){
+        this.startingchapter = StartingChapter;
+        this.endingchapter = EndingChapter;
+        this.saganame = SagaName;
     }
 }
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the Chapter number that you want to Know : ");
-        int chapter = scanner.nextInt();
-
-        DataOfChapter chapter1 = new DataOfChapter(chapter);
-        chapter1.checkChapterSaga();
+        int chapter_num = scanner.nextInt();
+        
+        Saga[] sagalist = new Saga[] {
+            new Saga(1, 61, "East Blue Saga"), 
+            new Saga(62, 135, "Arabasta Saga"),
+            new Saga(136, 206, "Sky Island Saga"),
+            new Saga(207, 135, "Water 7 Saga"),
+            new Saga(62, 135, "Thriller Bark Saga"),
+            new Saga(62, 135, "Summit War Saga"),
+        };
+        //DataOfChapter chapter1 = new DataOfChapter(chapter);
 
         scanner.close();
     }
